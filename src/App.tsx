f8c0a363b9c2a24be7bf0056e3b385e5267d2be5
Angelhpascual <Angelhpascual@gmail.com>
2025-02-06 17:28:31 +0100

    chore: configure eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 const App = () => {
-  return (
-    <div className="text-4xl text-yellow-200">Testing Tailwind</div>
-  )
-}
+  return <div className="text-4xl text-yellow-200">Testing Tailwind</div>;
+};
 
-export default App
+export default App;
