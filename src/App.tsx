f85f42eebfde855509a6263bf3d5ba49a5b468da
Angelhpascual <Angelhpascual@gmail.com>
2025-02-10 11:53:26 +0100

    feature: adding sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
+      <Sidebar />
       <Dashboard />
     </>
   );
